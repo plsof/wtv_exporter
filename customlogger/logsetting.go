5f9c7578da5bffeaffeef73583a5fa4e523813f0
plsof <1103901630@qq.com>
2020-03-15 23:25:24 +0800

    输出日志到文件

--- customlogger/logsetting.go
+++ customlogger/logsetting.go
@@ -1,1 +1,34 @@
 package customlogger
+
+import (
+	"log"
+	"os"
+	"sync"
+)
+
+type logger struct {
+	Filename string
+	Onfile   os.File
+	*log.Logger
+}
+
+var logInstance *logger
+var once sync.Once
+
+// start loggeando
+func GetInstance() *logger {
+	once.Do(func() {
+		logInstance = createLogger("logger.log")
+	})
+	return logInstance
+}
+
+func createLogger(fname string) *logger {
+	file, _ := os.OpenFile(fname, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0777)
+
+	return &logger{
+		Filename: fname,
+		Onfile:   *file,
+		Logger:   log.New(file, "wtv monitoring ", log.Ldate|log.Ltime|log.Lshortfile),
+	}
+}
