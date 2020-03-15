aa557c41132b87b640ea2480b890a73ad3ab280a
plsof <1103901630@qq.com>
2020-03-15 23:24:44 +0800

    优化func Collect

--- collector/wtv.go
+++ collector/wtv.go
@@ -3,7 +3,6 @@ package collector
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/prometheus/client_golang/prometheus"
 	"gopkg.in/yaml.v2"
@@ -13,12 +12,13 @@ import (
 	"strconv"
 	"sync"
 	"time"
+
+	"wtv_exporter/customlogger"
 )
 
 // 指标结构体
 type Metrics struct {
-	templateId string
-	metrics map[string]*prometheus.Desc
+	metrics *prometheus.Desc
 }
 
 /**
@@ -38,9 +38,7 @@ func newGlobalMetric(namespace string, metricName string, docString string) *pro
  */
 func NewMetrics(namespace string) *Metrics {
 	return &Metrics{
-		metrics: map[string]*prometheus.Desc{
-			"uuid_analysis_metric": newGlobalMetric(namespace, "uuid_analysis", "The description of uuid_analysis_metric"),
-		},
+		newGlobalMetric(namespace, "uuid_analysis", "The description of uuid_analysis_metric"),
 	}
 }
 
@@ -49,9 +47,7 @@ func NewMetrics(namespace string) *Metrics {
  * 功能：传递结构体中的指标描述符到channel
  */
 func (c *Metrics) Describe(ch chan<- *prometheus.Desc) {
-	for _, m := range c.metrics {
-		ch <- m
-	}
+	ch <- c.metrics
 }
 
 /**
@@ -59,27 +55,70 @@ func (c *Metrics) Describe(ch chan<- *prometheus.Desc) {
  * 功能：抓取最新的数据，传递给channel
  */
 func (c *Metrics) Collect(ch chan<- prometheus.Metric) {
-	setting := ParseYaml("./wtvConfig.yaml")
+	logger := customlogger.GetInstance()
+	//清空文件内容
+	os.Truncate(logger.Filename, 0)
+	logger.Onfile.Seek(0, 0)
+	//解析配置文件
+	setting := func () *Wtv {
+		config, err := ioutil.ReadFile("./wtvConfig.yaml")
+		if err != nil {
+			logger.Println("解析配置yaml文件出错 err=", err)
+		}
+		var setting Wtv
+		yaml.Unmarshal(config, &setting)
+		return &setting
+	}()
 	db, err := sql.Open("sqlite3", setting.DB)
 	defer db.Close()
 	if err != nil {
-		fmt.Printf("连接数据库失败 %v", err)
-		os.Exit(1)
+		logger.Fatalln("数据库连接失败, err=", err)
 	}
+	// 获取每天凌晨的时间戳
+	zeroTime := func () int64 {
+		t := time.Now()
+		zeroTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
+		return zeroTime
+	}()
 	var wg sync.WaitGroup
+	var uuid string
 	for _, id := range setting.TemplateId {
+		//获取getChannels接口数据并反序列化
+		channels := func () []Channel {
+			resp, err := http.Get(setting.GetChannelsUrl + "?" + "templateId=" + id)
+			if err != nil {
+				logger.Fatalln("获取getChannels接口失败 err=", err)
+			}
+			defer resp.Body.Close()
+			b, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				logger.Fatalln("读取getChannels接口内容失败 err=", err)
+			}
+			var slice []Channel
+			err = json.Unmarshal(b, &slice)
+			if err != nil {
+				logger.Fatalln("getChannels接口反序列化失败 err=", err)
+			}
+			return slice
+		}()
 		rows, err := db.Query("select uuid from w" + id)
 		if err != nil {
-			fmt.Printf("查询失败 err=%v\n", err)
+			logger.Println("查询失败 err=", err)
 		}
 		for rows.Next(){
-			var uuid string
 			err = rows.Scan(&uuid)
 			if err !=nil{
-				fmt.Println(err)
+				logger.Println("获取uuid失败 err=", err)
+			}
+			var offset int8
+			offset = 0
+			for _, c := range channels {
+				if uuid == c.Uuid {
+					offset +=1
+				}
 			}
 			wg.Add(1)
-			go VerifyData(setting.GetAllDayUrl, id, uuid, wg.Done, ch)
+			go VerifyData(setting.GetAllDayUrl, id, uuid, offset, zeroTime, wg.Done, ch)
 		}
 		rows.Close()
 	}
@@ -88,9 +127,14 @@ func (c *Metrics) Collect(ch chan<- prometheus.Metric) {
 
 type Wtv struct {
 	TemplateId []string `yaml:"templateid"`
-	GetAllDayUrl string `yaml:"getallday_url"`
 	GetChannelsUrl string `yaml:"getchannels_url"`
+	GetAllDayUrl string `yaml:"getallday_url"`
 	DB string `yaml:"db"`
+	LogPath string `yaml:"logpath"`
+}
+
+type Channel struct {
+	Uuid string `json:uuid`
 }
 
 type Program struct {
@@ -105,15 +149,37 @@ type allPrograms struct {
 	Programs []Program `json:"programs"`
 }
 
-func VerifyData(url string, id string, uuid string, done func(), ch chan<- prometheus.Metric) {
+func VerifyData(url string, id string, uuid string, offset int8, zeroTime int64, done func(), ch chan<- prometheus.Metric) {
 	defer done()
-	p := httpAllDay(url + "?" + "templateId=" + id + "&" + "uuid=" + uuid)
+	logger := customlogger.GetInstance()
+	if offset == 0 {
+		logger.Printf("%s %s programs lack\n", id, uuid)
+		ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(-1), id, uuid)
+		return
+	}
+	// 获取getAllDayPrograms接口数据并反序列化
+	p := func () []allPrograms {
+		resp, err := http.Get(url + "?" + "templateId=" + id + "&" + "uuid=" + uuid)
+		if err != nil {
+			logger.Fatalln("获取getAllDayPrograms接口失败 err=", err)
+		}
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			logger.Fatalln("读取getAllDayPrograms接口内容失败 err=", err)
+		}
+		var slice []allPrograms
+		err = json.Unmarshal(b, &slice)
+		if err != nil {
+			logger.Fatalln("getAllDayPrograms接口反序列化失败 err=", err)
+		}
+		return slice
+	}()
 	if len(p) == 0 {
-		fmt.Printf("%s %s programs null\n", id, uuid)
+		logger.Printf("%s %s programs null\n", id, uuid)
 		ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(1), id, uuid)
 		return
 	}
-	zeroTime := getTime()
 	programTime, _ := strconv.ParseInt(p[0].PlayDate,10,64)
 	if programTime >= zeroTime {
 		var futureTime []string
@@ -128,7 +194,7 @@ func VerifyData(url string, id string, uuid string, done func(), ch chan<- prome
 				playTime = append(playTime, v.EndTime)
 			case "replay":
 			default :
-				fmt.Println("[0]没有匹配到")
+				logger.Println("[0]没有匹配到")
 			}
 		}
 		if len(playTime) == 0 {
@@ -141,13 +207,13 @@ func VerifyData(url string, id string, uuid string, done func(), ch chan<- prome
 					playTime = append(playTime, v.EndTime)
 				case "replay":
 				default:
-					fmt.Println("[1]没有匹配到")
+					logger.Println("[1]没有匹配到")
 				}
 			}
 		}
 		// 23:30的直播信息在p[1], 第一个预加载节目单可能也在 所以要遍历p[0] p[1]
 		if len(playTime) == 0 {
-			fmt.Printf("%s %s programs now miss\n", id, uuid)
+			logger.Printf("%s %s programs now miss\n", id, uuid)
 			ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(2), id, uuid)
 			return
 		}
@@ -159,64 +225,30 @@ func VerifyData(url string, id string, uuid string, done func(), ch chan<- prome
 		}
 		switch len(trueFutureTime) {
 		case 0:
-			fmt.Printf("%s %s programs future miss\n", id, uuid)
+			logger.Printf("%s %s programs future miss\n", id, uuid)
 			ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(3), id, uuid)
 		case 1,2,3:
-			fmt.Printf("%s %s programs future less\n", id, uuid)
+			logger.Printf("%s %s programs future less\n", id, uuid)
 			ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(4), id, uuid)
 		case 4:
 			if playTime[0] == trueFutureTime[3] && trueFutureTime[2] == trueFutureTime[1] {
-				fmt.Printf("%s %s programs ok\n", id, uuid)
+				logger.Printf("%s %s programs ok\n", id, uuid)
 				ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(0), id, uuid)
 			} else if playTime[0] < trueFutureTime[3] || trueFutureTime[2] < trueFutureTime[1] {
-				fmt.Printf("%s %s programs future uncontinuity\n", id, uuid)
+				logger.Printf("%s %s programs future uncontinuity\n", id, uuid)
 				ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(5), id, uuid)
 			} else {
-				fmt.Printf("%s %s programs future overlap\n", id, uuid)
+				logger.Printf("%s %s programs future overlap\n", id, uuid)
 				ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(6), id, uuid)
 			}
 		}
 	} else {
-		fmt.Printf("%s %s programs today miss\n", id, uuid)
+		logger.Printf("%s %s programs today miss\n", id, uuid)
 		ch <-prometheus.MustNewConstMetric(newGlobalMetric("wtv", "uuid_analysis", "The description of uuid_analysis_metric"), prometheus.GaugeValue, float64(2), id, uuid)
 	}
 }
 
-func ParseYaml(path string) *Wtv {
-	config, err := ioutil.ReadFile(path)
-	if err != nil {
-		fmt.Println("error=", err)
-	}
-	var setting Wtv
-	yaml.Unmarshal(config, &setting)
-	return &setting
-}
 
-// 获取每天凌晨的时间戳
-func getTime() int64 {
-	t := time.Now()
-	zeroTime := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location()).Unix()
-	return zeroTime
-}
 
-// 获取接口数据并反序列化
-func httpAllDay(url string) []allPrograms {
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-		os.Exit(1)
-	}
-	defer resp.Body.Close()
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-		os.Exit(1)
-	}
-	//fmt.Printf("%s", b)
-	var slice []allPrograms
-	err = json.Unmarshal(b, &slice)
-	if err != nil {
-		fmt.Printf("unmarshal err=%v\n", err)
-	}
-	return slice
-}
+
+
